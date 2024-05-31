83caa1abfafe2c504deef7883bbdee420593cf5d
LilHunion <PinkPanther.dv@gmail.com>
2024-05-31 14:40:11 -0600

    Initial Commit

---
+++ Cooking App/Cooking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cooking_AppApp.swift
+//  Cooking App
+//
+//  Created by Devon Villareal on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Cooking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
